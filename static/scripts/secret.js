081417a61ac38839afa6ae399e6ea7bbbc101b69
theoharfordtapp <theoharfordtapp@gmail.com>
2024-08-24 15:32:14 +0100

    Revealed secrets

---
+++ static/scripts/secret.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAVD7Zc-XKtjfDg4pjxL8b9QmuwxEY0eHM",
+    authDomain: "adaptiveanarchybase.firebaseapp.com",
+    projectId: "adaptiveanarchybase",
+    storageBucket: "adaptiveanarchybase.appspot.com",
+    messagingSenderId: "256141998502",
+    appId: "1:256141998502:web:eaab803dab5b9c723a3767",
+    measurementId: "G-N8M3QYTZZY"
+};
+
+export { firebaseConfig }
